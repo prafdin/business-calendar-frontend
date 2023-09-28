95db56756bc9dac42e44c496d1899935d96d0cfc
VolumeCore <volumejke@gmail.com>
2023-09-28 11:56:10 +0400

    Add header and announcement components

--- business-calendar/src/app/app.module.ts
+++ business-calendar/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AnnouncementsComponent } from './announcements/announcements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AnnouncementsComponent
   ],
   imports: [
     BrowserModule,
