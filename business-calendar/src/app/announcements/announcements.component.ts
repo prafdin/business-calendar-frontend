63ba2c5e65f6e120b04f9b6dbdb4466acd15a9e6
VolumeCore <volumejke@gmail.com>
2023-09-28 13:00:54 +0400

    Add slider to announcements component

--- business-calendar/src/app/announcements/announcements.component.ts
+++ business-calendar/src/app/announcements/announcements.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild, ViewEncapsulation } from '@angular/core';
+import KeenSlider, { KeenSliderInstance } from "keen-slider"
 
 @Component({
-  selector: 'announcements',
-  templateUrl: './announcements.component.html',
-  styleUrls: ['./announcements.component.less']
+    selector: 'announcements',
+    templateUrl: './announcements.component.html',
+    styleUrls: ['./announcements.component.less'],
+    encapsulation: ViewEncapsulation.None
 })
-export class AnnouncementsComponent {
+export class AnnouncementsComponent implements AfterViewInit, OnDestroy {
+    @ViewChild("sliderRef") sliderRef!: ElementRef<HTMLElement>
 
+    slider: KeenSliderInstance | null = null;
+
+    ngAfterViewInit() {
+        this.slider = new KeenSlider(this.sliderRef.nativeElement);
+    }
+
+    ngOnDestroy() {
+        if (this.slider) {
+            this.slider.destroy();
+        }
+    }
 }
