83f0b426d398166b8309657f68b3829d3059c1c7
VolumeCore <volumejke@gmail.com>
2023-09-28 11:56:37 +0400

    Add header and announcement components

---
+++ business-calendar/src/app/announcements/announcements.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'announcements',
+  templateUrl: './announcements.component.html',
+  styleUrls: ['./announcements.component.less']
+})
+export class AnnouncementsComponent {
+
+}
