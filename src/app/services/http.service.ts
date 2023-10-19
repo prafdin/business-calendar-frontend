fdfee000bbdcc6bb5a8a722e40973cab980ead6a
vika lo <victoria.tchekushkina@yandex.ru>
2023-10-19 14:53:55 +0400

    added elements to the announcements, edit toolbar and calendar, format time in card

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import * as moment from "moment/moment";
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class HttpService {
 
-    constructor(private http: HttpClient) {
-    }
+  constructor(private http: HttpClient) {
+  }
 
-    public getEvents(date: string, offset: number): Observable<any> {
-        return this.http.get(`/Event/targetdate=${date}&offset=${offset}`);
-    }
+  public getEvents(date: string, offset: number): Observable<any> {
+    return this.http.get(`/Event/targetdate=${date}&offset=${offset}`);
+  }
+
+  // public getAnnouncements(): Observable<any> {
+  //   return this.http.get(`/Event/currentdate=${moment().add(new Date().getTimezoneOffset()*(-1), "minutes").toISOString()}`);
+  // }
+
 }
