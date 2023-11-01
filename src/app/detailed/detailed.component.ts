b4595f32cbe8c9f972e0132cc2ba7df6e2dfd355
rote0222 <roman.tepechin@netcracker.com>
2023-11-01 18:05:12 +0400

    01 11

--- src/app/detailed/detailed.component.ts
+++ src/app/detailed/detailed.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from "@angular/router";
 
 @Component({
